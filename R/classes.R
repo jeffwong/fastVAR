5e573b0f909c71d0c53dbbfa511a7faf1c3bcdd3
jeffwong <jeff.ct.wong@gmail.com>
2012-12-16 00:15:09 -0800

    add classes for GVAR and GVARX

--- R/classes.R
+++ R/classes.R
@@ -1,11 +1,15 @@
 setClass("fastVAR.VARX", representation = "list", S3methods = T)
 setClass("fastVAR.SparseVARX", representation = "list", S3methods = T)
+setClass("fastVAR.GVARX", representation = "list", S3methods=T)
 setClass("fastVAR.VARXZ", representation = "list", S3methods = T)
 
 setClass("fastVAR.VAR", representation = "list", S3methods = T)
 setClass("fastVAR.SparseVAR", representation = "list", S3methods = T)
+setClass("fastVAR.GVAR", representation = "list", S3methods=T)
 setClass("fastVAR.VARZ", representation = "list", S3methods = T)
 
 setClass("fastVAR.RidgePath", representation = "list", S3methods=T)
 
 setClass("fastVAR.fastMlm", representation = "list", S3methods=T)
+setClass("fastVAR.graphicalLm", representation = "list", S3methods=T)
+
