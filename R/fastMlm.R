ed5441ad725c3ea7e74bed601ce71d647a2bd992
jeffwong <jeff.ct.wong@gmail.com>
2012-12-15 16:32:07 -0800

    fix bug with wrapper

--- R/fastMlm.R
+++ R/fastMlm.R
@@ -2,7 +2,7 @@
 fastMlm = function(X, Y, weights = NULL) {
     if (!is.null(weights)) {X = scale.rows(X, sqrt(weights)); Y = scale.rows(Y, sqrt(weights))}
     mlm = fast_mlm(X, Y)
-    return (structure(mlm), class = "fastVAR.fastMlm")
+    return (structure(mlm, class = "fastVAR.fastMlm"))
 }
 
 coef.fastVAR.fastMlm = function(fastMlm) {
